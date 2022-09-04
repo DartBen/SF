df090304d76be6e5bef864be86be2e62fcad0717
Dmitry <ryabchikk@rambler.ru>
2022-09-04 17:06:41 +0300

    FirstApp

---
+++ FirstApplication/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Как вас зовут?");
+        //string? Name = null;
+        string? Name=Console.ReadLine();
+        Console.WriteLine("Здраствуйте,{0}", Name);
+        Console.WriteLine("Я инженер-программист АСУТП. Хочу сменить несколько сменить профиль");
+    }
+}
